e1820fcb7a7f4c512e678a293ebb8320bae3418a
andr1anovv <an4lex@gmail.com>
2024-09-15 00:13:24 +0300

    Set the route to each page

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,14 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\RedirectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-Route::get('/services', function () {
-    return view('services');
-})->name('services');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-
-Route::get('/portfolio', function () {
-    return view('portfolio');
-})->name('portfolio');
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/services', [PageController::class, 'services'])->name('services');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+Route::get('/portfolio', [PageController::class, 'portfolio'])->name('portfolio');
+Route::get('/page/{slug}', [PageController::class, 'showPage'])->name('showPage');
+// TASK 4. Using the Single Action Controller
+Route::get('/redirect', RedirectController::class);
