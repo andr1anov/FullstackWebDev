3fda9939fc41b9af45de4c5468593cb7fc048c16
andr1anovv <an4lex@gmail.com>
2024-09-14 19:19:14 +0300

    Added a route to each page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+})->name('portfolio');
